56dba81dfa308df3edac5515fbce23602870f6db
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-27 12:30:13 +0300

    import the enviroment file plus created a function for user info and repo

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import {environment} from '../environments/environment';
+import {map} from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
   private username:string;
-  operator = "https://api.github.com/users/"
-  access = "?access_token=abff63ac7da65497ddc76741f416c57e7d3b7414"
 
   constructor(private http:Http) {
-    console.log()
-    this.username = 'abbiemurugi'
+    this.username = 'nyagaG'
    }
    getrepoInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username + this.access)
-     .map(res => res.json());
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + environment.clientid + "&client_secret=" + environment.clientsecret)
+     .pipe(map(res => res.json()));
    }
+   getrepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "repos?client_id=" + environment.clientid + "&client_secret=" + environment.clientsecret)
+    .pipe(map(res => res.json()));
   }
+ 
+  updateRepo(username:string){
+    this.username = username;
+ }
+
+}
