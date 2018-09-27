e5c871234c70de1560bfbc9b77c1cdc0f1a64ebf
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-27 12:19:44 +0300

    added Readme.md

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
+  private username:string;
+  operator = "https://api.github.com/users/"
+  access = "?access_token=abff63ac7da65497ddc76741f416c57e7d3b7414"
 
-  constructor() { }
-}
+  constructor(private http:Http) {
+    console.log()
+    this.username = 'abbiemurugi'
+   }
+   getrepoInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + this.access)
+     .map(res => res.json());
+   }
+  }
