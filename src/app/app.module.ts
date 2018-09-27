9f331286134f58b20717820bce64e1f0a2b0edb0
abbiemurugi <abigaelmurugi@gmail.com>
2018-09-27 12:21:58 +0300

    import formModule, httpmodule, reposervice plus added repocomponents in the declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { RepoService } from './repo.service'
 import { AppComponent } from './app.component';
-import { RepoComponent } from './repo/repo.component'
+import { RepoComponent } from './repo/repo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepoComponent
+    RepoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [RepoService],
   bootstrap: [AppComponent],
